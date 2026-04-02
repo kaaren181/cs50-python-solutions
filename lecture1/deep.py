e80607b2d599f1bd489d13f7a357bbac416e6283
kaaren181 <kaaren.dastan@gmail.com>
2026-04-02 13:00:48 -0700

    Lecture 1 - Deep Thought solution

---
+++ lecture1/deep.py
@@ -0,0 +1,11 @@
+x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+x = x.casefold()
+x = x.strip()
+
+match x:
+    case "42":
+        print("Yes")
+    case "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
