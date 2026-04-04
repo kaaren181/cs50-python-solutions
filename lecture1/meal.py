a5080bd737d5cf4d2eb9fc1299bfe190d83057a2
kaaren181 <kaaren.dastan@gmail.com>
2026-04-03 22:51:30 -0700

    Lecture 1 - Meal Time solution

---
+++ lecture1/meal.py
@@ -0,0 +1,22 @@
+def main():
+    meal = input("What time is it? ")
+    x = convert(meal)
+    if 7 <= x <= 8:
+        print("breakfast time")
+    elif 12 <= x <= 13:
+        print("lunch time")
+    elif 18 <= x <= 19:
+        print("dinner time")
+
+
+def convert(y):
+    h, m = y.split(":")
+    h = float(h)
+    m = float(m)
+    m = m / 60
+    z = h + m
+    return z
+
+
+if __name__ == "__main__":
+    main()
